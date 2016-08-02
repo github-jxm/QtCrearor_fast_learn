401b1121a20cab5b453e1b952ec7e21ab3109fd4
jiang_xmin <jiang_xmin@massclouds.com>
2016-08-02 22:07:40 +0800

    xxxx

--- src/16/16-1/modelView1/main.cpp
+++ src/16/16-1/modelView1/main.cpp
@@ -7,23 +7,24 @@
 int main(int argc, char *argv[])
 {
     QApplication app(argc, argv);
-    // 创建文件系统模型
-    QFileSystemModel model;
-    // 指定要监视的目录
-    model.setRootPath(QDir::currentPath());
+
+    /* 1. 创建　模型 model  */
+
+    QFileSystemModel model;// 创建文件系统模型
+    model.setRootPath(QDir::currentPath());// 指定要监视的目录
 
 
-    // 创建树型视图
-    QTreeView tree;
-    // 为视图指定模型
-    tree.setModel(&model);
-    // 指定根索引
-    tree.setRootIndex(model.index(QDir::currentPath() ));
+    /* 2. 创建　视图 view  */
 
-    // 创建列表视图
-    QListView list;
-    list.setModel(&model);
-    list.setRootIndex(model.index(QDir::currentPath()));
+    QTreeView tree;            // 创建树型视图
+    tree.setModel(&model);// 为视图指定模型
+    tree.setRootIndex(model.index(QDir::currentPath() ));// 指定根索引
+
+    QListView list;               // 创建列表视图
+    list.setModel(&model);// 为视图指定模型
+    list.setRootIndex(model.index(QDir::currentPath()));// 指定根索引
+
+    /*   3. 窗口　显示 */
 
     tree.show();
     list.show();
